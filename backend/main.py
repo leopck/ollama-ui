7c8253a1afa84bf8fe92fb4e8bfca994ee187f5d
Stanley <sckphoong@habana.ai>
2024-12-08 19:27:31 -0800

    llama 3.2 model instead

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import ollama
 
 def get_response(user_input):
     stream = ollama.chat(
-        model='llama3',
+        model='llama3.2',
         messages=[{'role': 'user', 'content': user_input}],
         stream=True,
     )
